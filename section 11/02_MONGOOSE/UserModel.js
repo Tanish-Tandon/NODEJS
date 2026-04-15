d72e4312df31cc279c43a7763821be7a0720f855
Tanish-Tandon <tandontanish99@gmail.com>
2026-04-15 20:14:57 +0530

    DEEP DIVE INTO MONGOOSE FUNCTIONS METHODS SCHEMA

--- section 11/02_MONGOOSE/UserModel.js
+++ section 11/02_MONGOOSE/UserModel.js
@@ -5,15 +5,77 @@
 
 
 
-import mongoose from "mongoose";
+import mongoose ,{Schema, SchemaTypes} from "mongoose";
 
-const UserModel=mongoose.model("User",{name:String,age:Number});
+console.log("START USERMODEL.JS FILE");
+
+// const userSchema={name:String,age:Number}
+
+
+
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:[true,"Name is required Please provide name"],
+        minLength:[5,"Name must be at least 5 characters long"],
+        trim:true
+        
+
+    },
+    age:{
+        type:Number,
+        required:[true,"Age is required Please provide age"],
+        min:[18,"Age must be at least 18"],
+    },
+
+     email:{
+        type:String,
+        required:[true,"Email is required Please provide email"],
+        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Please provide a valid email address"],
+        lowercase:true,
+        trim:true,
+    },
+
+    hobby:[String],
+    parentId:{
+        type:Schema.Types.ObjectId,
+        required:function(){
+            return this.age < 22;
+        },
+        default:null
+    }
+},
+
+{
+    strict:"throw",
+
+
+
+    timestamps:true,
 
 
 
 
 
-const data= await UserModel.findOne({name:"ANK"});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+const UserModel=mongoose.model("User",userSchema);
+
+
+
+const data= await UserModel.insertOne({name:"THE RIT TANDON",age:21,email:"tandon123@gmail.com",hobby:["coding"],parentId:"69dfa0ce536ed0638fbc9ab6"});
 
 
 console.log(data);
--- section 11/02_MONGOOSE/app.js
+++ section 11/02_MONGOOSE/app.js
@@ -5,7 +5,3 @@ import "./UserModel.js";
 
 
 console.log("APP.JS FILE EXECUTED");
-
-// import sbse phele excecute hote hain to db.js aur UserModel.js ke andar ke code ko execute karne ke liye side effects import ka use kiya hai.
-
-// db object mongoose ke andar create hota hai jab mongoose.connect() function call hota hai. isliye db object ko directly import nahi kar sakte hain. isliye humne db.js file ko side effects import kiya hai taki uske andar ka code execute ho jaye aur db object create ho jaye.
