39d19ee2a32b4dff35f6578d4f1bc7feca2c4750
Tanish-Tandon <tandontanish99@gmail.com>
2026-04-18 05:47:45 +0530

    THENABLE OBJECT

---
+++ section 11/02_MONGOOSE/script.js
@@ -0,0 +1,73 @@
+// const myObj={
+//     hello(resolve){
+//         setTimeout(()=>{
+//             resolve("RESOLVE FUNCTION CALLED");
+//         },2000);
+//     },
+// };
+
+
+
+
+// Thenable = object with .then(resolve, reject)
+// await → Promise OR thenable dono handle karta hai
+// Tera object abhi thenable nahi hai
+
+
+
+
+
+
+// const myObj={
+//     then(resolve){
+//         setTimeout(()=>{
+//             resolve("RESOLVE FUNCTION CALLED");
+//         },2000);
+//     },
+// };
+
+
+
+// const data=await myObj;
+// console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const myObj={
+    then(resolve){
+       h1.addEventListener("click",()=>{
+        resolve("RESOLVE FUNCTION CALLED");
+       });
+    },
+};
+
+
+
+const data=await myObj;
+console.log(data);
+
+
+
+console.log(await myObj);
+
+console.log(await myObj);
+
+console.log(await myObj);
+
+console.log(await myObj);
